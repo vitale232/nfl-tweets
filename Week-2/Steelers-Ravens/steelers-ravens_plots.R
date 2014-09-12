6e41eb7c538addc4c104b318acb500603ec1b434
vitale232 <vitale232@gmail.com>
2014-09-12 08:52:01 -0700

    create script to plot maps

---
+++ Week-2/Steelers-Ravens/steelers-ravens_plots.R
@@ -0,0 +1,71 @@
+#--------------------------------
+# Name:         steelers-ravens_plots.R
+# Purpose:      Plot the coords saved in the CSV file from Python
+# Author:       Andrew Vitale  vitale232@gmail.com
+# Created       2014/09/12
+# R:            3.1.1
+#--------------------------------
+
+library(raster)
+library(rgdal)
+library(maps)
+library(maptools)
+
+setwd('~/Google Drive/twitter/Week-2/Steelers-Ravens/')
+
+steelers = read.csv('steelers_coords.csv')
+coordinates(steelers) = ~long+lat
+proj4string(steelers) = '+proj=longlat +datum=WGS84'
+
+ravens = read.csv('ravens_coords.csv')
+coordinates(ravens) = ~long+lat
+proj4string(ravens) = '+proj=longlat +datum=WGS84'
+
+#### Upload the map of the USA from the maps package 
+the_map = map("state", fill=TRUE, col="transparent", plot=FALSE)
+
+#### Coerce the map to a SpatialPolgons object
+IDs = sapply(strsplit(the_map$names, ":"), function(x) x[1])
+the_map_sp = map2SpatialPolygons(the_map, IDs=IDs,
+                                 proj4string=CRS("+proj=longlat +datum=WGS84"))
+
+r = raster(nrow=1080, ncol=2160)
+
+steelers_count = rasterize(steelers, r, fun='count')
+ravens_count = rasterize(ravens, r, fun='count')
+
+steelers_count = crop(steelers_count, the_map_sp)
+ravens_count = crop(ravens_count, the_map_sp)
+
+aea = '+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs'
+the_map_sp = spTransform(the_map_sp, CRS(aea))
+steelers_count = projectRaster(steelers_count, crs=aea)
+ravens_count = projectRaster(ravens_count, crs=aea)
+
+steelers_count[which(!is.na(values(steelers_count)))] = 
+  round(values(steelers_count)[-which(is.na(values(steelers_count)))], 0)
+ravens_count[which(!is.na(values(ravens_count)))] = 
+  round(values(ravens_count)[-which(is.na(values(ravens_count)))], 0)
+
+steelers_cols = colorRampPalette(c('black', 'yellow'))
+ravens_cols  = colorRampPalette(c('darkorchid4', 'white'))
+
+png('./steelers_map.png', width=10, height=7, units='in', res=300)
+#x11(height=7, width=10)
+par(mar=c(5, 0, 4, 2) + 0.1)
+plot(the_map_sp, axes=FALSE, col='azure2', border='black')
+plot(steelers_count, add=TRUE, col=steelers_cols(255))
+plot(the_map_sp, add=TRUE, col=NA, border='black')
+title("Tweets Returned for Search of 'Steelers'", cex=1.25)
+mtext('Recorded from 15:12 2014-09-11 to 03:12 PDT 2014-09-12', cex=1.25)
+dev.off()
+
+png('./ravens_map.png', width=10, height=7, units='in', res=300)
+plot(the_map_sp, axes=FALSE, col='azure2', border='black')
+plot(ravens_count, add=TRUE, col=ravens_cols(255))
+plot(the_map_sp, add=TRUE, col=NA, border='black')
+title("Tweets Returned for Search of 'Ravens'", cex=1.25)
+mtext('Recorded from 15:12 2014-09-11 to 03:12 PDT 2014-09-12', cex=1.25)
+dev.off()
+
+
